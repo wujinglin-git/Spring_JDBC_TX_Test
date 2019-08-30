28bc4ebd5d44a5f2fa4811a7ac722e12298ab733
Administrator <https://github.com/wujinglin-git/wshop.git>
2019-08-30 22:10:28 +0800

    关于jdbcTemplate主要功能的DEMO

---
+++ src/main/java/com/wjl/App.java
@@ -0,0 +1,13 @@
+package com.wjl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/wjl/pojo/UserInfo.java
@@ -0,0 +1,56 @@
+package com.wjl.pojo;
+
+/**
+ * @Author: wugege
+ * @Date: 2019/8/30 16:30
+ * 一给窝哩 giao giao 呀吼
+ * 4
+ */
+public class UserInfo {
+    private int id;
+    private  String name;
+    private  String password;
+    private  String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
